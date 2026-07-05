1f19b65ad381d21ab01d417d308c5c802371c724
Serviteca VJ <tu_correo@ejemplo.com>
2026-07-05 01:44:42 -0400

    onectar PWA a base de datos principal NEUMATICOS VJ

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -3,17 +3,19 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBcTHCJ24N-W3r3W-0PqKe4HgIFsPmO63Q",
-  authDomain: "serviteca-vj.firebaseapp.com",
-  projectId: "serviteca-vj",
-  storageBucket: "serviteca-vj.firebasestorage.app",
-  messagingSenderId: "349430767084",
-  appId: "1:349430767084:web:4ead2192211889fb0834c9",
-  measurementId: "G-Y5KJSM5FVG"
+  apiKey: "AIzaSyAf5ZDCPfN-tqjgWHfV3sGmLuzf7vk5McI",
+  authDomain: "neumaticos-vj.firebaseapp.com",
+  databaseURL: "https://neumaticos-vj-default-rtdb.firebaseio.com",
+  projectId: "neumaticos-vj",
+  storageBucket: "neumaticos-vj.firebasestorage.app",
+  messagingSenderId: "1094394431570",
+  appId: "1:1094394431570:web:09fd91c4825576d90cf78b",
+  measurementId: "G-WPMSMVYDR8"
 };
 
+// Inicializamos Firebase con tu base de datos principal
 const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
 
-// ¡Esta es la línea vital que hace que se conecte a tu inventario!
+// Exportamos la conexión a tu bodega real
 export const db = getFirestore(app);
